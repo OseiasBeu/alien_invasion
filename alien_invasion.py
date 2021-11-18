c7a8d54e9802e53baf918e82097651abbd2f51ed
Oseias Beu <oseiasbeu@outlook.com>
2021-11-17 23:26:15 -0300

    Criar game functions para refatoracao do codigo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
     # Inicializa o jogo e cria um objeto para a tela 
@@ -20,14 +19,8 @@ def run_game():
 
     # Inicia o laço principal do jogo
     while True:
+        gf.check_events()
+        gf.update_screen(ai_settings,screen,ship)
         # Observa eventos de teclado e de mouse
         # Redesenha a tela a cada passagem pelo laço
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # Deixa a tela mais recente visível
-        pygame.display.flip()
-
 run_game()
