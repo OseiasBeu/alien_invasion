536b5e47ef3d674bd8045dc0d8b228e93f2da2d7
Oseias Beu <oseiasbeu@outlook.com>
2021-11-18 15:48:23 -0300

    Aumentar a valocidade da nave

--- settings.py
+++ settings.py
@@ -6,4 +6,7 @@ class Settings():
         self.screen_width = 1100
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        # Configurações da espaçonave
+        self.ship_speed_factor = 1.5
     
