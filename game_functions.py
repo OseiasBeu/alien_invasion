20b2d6aef4de7584459664145a83424597f5bc82
Oseias Beu <oseiasbeu@outlook.com>
2021-11-25 02:14:45 -0300

    Refatoração do jogo

--- game_functions.py
+++ game_functions.py
@@ -11,11 +11,15 @@ def check_keydown_events(event,ai_settings, screen,ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings,screen,ship, bullets)
+
+def fire_bullet(ai_settings,screen,ship, bullets):
+    """ Dispara um projétil se o limite ainda não foi alcançado."""
         # Cria um novo projétil e o adiciona ao grupo de projéteis
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings,screen,ship)
-            bullets.add(new_bullet)
-    
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings,screen,ship)
+        bullets.add(new_bullet)
+        
 def check_keyup_events(event, ship):
     """Responde a solturas de tecla."""
     if event.key == pygame.K_RIGHT:
